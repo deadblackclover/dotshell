7705cc689ebfd6aacfa60381e6d5b8ed2f00abe1
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-02-01 02:11:56 +0700

    Added the ability to specify the server host

--- cmd/dotshell/main.go
+++ cmd/dotshell/main.go
@@ -8,16 +8,21 @@ import (
 	"github.com/deadblackclover/dotshell/internal/handlers"
 )
 
+var host string
 var port = "8080"
 
 func main() {
+	if envHost := os.Getenv("HOST"); envHost != "" {
+		host = envHost
+	}
+
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		port = envPort
 	}
 
 	http.HandleFunc("/", handlers.IndexHandler)
 
-	log.Printf("Server is running on port: %s\n", port)
+	log.Printf("Server is running at %s:%s\n", host, port)
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
 }
