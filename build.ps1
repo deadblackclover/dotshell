7f9a2db7f4e0e4b47ad684c88c81ba96cb389a04
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-02-03 05:13:16 +0700

    Added tasks in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,29 @@
-go build -o ./build/ ./cmd/dotshell
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$Task = "build"
+)
+
+function Fmt {
+    gofmt -s -w ./.
+}
+
+function Build {
+    go build -o ./build/ ./cmd/dotshell
+}
+
+function Run {
+    go run ./cmd/dotshell
+}
+
+function RunPublic {
+    $env:HOST="0.0.0.0"
+    Run
+}
+
+switch ($Task) {
+    "fmt" { Fmt }
+    "build" { Build }
+    "run" { Run }
+    "run-public" { RunPublic }
+    Default { Write-Host "Unknown task: $Task" }
+}
