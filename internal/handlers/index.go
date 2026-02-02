2f5f2145fdeb5704a09734ee8b98bfe037e84d86
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-02-03 04:45:49 +0700

    Updated index.go
    Getting the path, system data, and files

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -2,23 +2,57 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 
 	. "github.com/deadblackclover/dotshell/internal/utils"
 )
 
 type Data struct {
-	System System
+	System *System
+	File   *File
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	d := getData()
-	t, _ := template.ParseFiles("internal/templates/index.html")
-	t.Execute(w, d)
+	// Not processing /favicon.ico
+	if r.URL.Path == "/favicon.ico" {
+		return
+	}
+
+	log.Printf("URL=%s", r.URL.String())
+
+	// Get all the values
+	params := &GetParams{}
+	params.Parse(r)
+
+	// Collecting data
+	data := &Data{
+		System: &System{},
+	}
+	data.System.Init()
+
+	// Getting data from the path
+	if file, err := NewFile(params.Path); err == nil {
+		data.File = file
+	} else {
+		log.Println(err)
+	}
+
+	// Working with a template
+	t, err := template.ParseFiles("internal/templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, data)
 }
 
-func getData() Data {
-	var data Data
-	data.System = GetSystemData()
-	return data
+type GetParams struct {
+	Path string
+	Cmd  string
+}
+
+func (p *GetParams) Parse(r *http.Request) {
+	query := r.URL.Query()
+	p.Path = query.Get("path")
+	p.Cmd = query.Get("cmd")
 }
